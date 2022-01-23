31786461c4d06ce1c559129f3f19d321eb9f5d73
mrobimail <98216643+mrobimail@users.noreply.github.com>
2022-01-23 15:09:00 +0100

    Repair first day result seconds

---
+++ app.js
@@ -0,0 +1,9 @@
+const calculateDueDate = require('./services/dueDateCalculator.js');
+
+const input = {
+    submitDate: '2022-01-21T14:00:00.000Z',
+    turnaroundTime: 8,
+}
+
+console.log('# Input: ', input.submitDate, input.turnaroundTime);
+console.log('# Result: ', calculateDueDate(input.submitDate, input.turnaroundTime));
